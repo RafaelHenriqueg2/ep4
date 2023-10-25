8ff5eeed6a621b69fd25d4fdd4f213f788bb0465
Rafael Henrique <127903349+RafaelHenriqueg2@users.noreply.github.com>
2023-10-25 08:48:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 function botar() {
-  var respostaUsuario1 = document.querySelector('input[name="resposta"]:checked');
+  var respostaUsuario1 = document.querySelector('input[name="resposta1"]:checked');
   var respostaUsuario2 = document.querySelector('input[name="resposta2"]:checked');
   var respostaUsuario3 = document.querySelector('input[name="resposta3"]:checked');
   var respostaUsuario4 = document.querySelector('input[name="resposta4"]:checked');
+  var respostaUsuario5 = document.querySelector('input[name="resposta1"]:checked');
+  var respostaUsuario6 = document.querySelector('input[name="resposta2"]:checked');
+  var respostaUsuario7 = document.querySelector('input[name="resposta3"]:checked');
+  var respostaUsuario8 = document.querySelector('input[name="resposta4"]:checked');
+  var respostaUsuario9 = document.querySelector('input[name="resposta3"]:checked');
+  var respostaUsuario10 = document.querySelector('input[name="resposta4"]:checked');
 
   if (respostaUsuario1 && respostaUsuario2 && respostaUsuario3 && respostaUsuario4) {
-    if (respostaUsuario1.value === "correto") {
+    if (respostaUsuario1.value == "correta") {
       document.getElementById("resultado1").innerHTML = "acertou";
     } else {
       document.getElementById("resultado1").innerHTML = "errou";
     }
 
-    if (respostaUsuario2.value === "correto") {
+    if (respostaUsuario2.value == "correta") {
       document.getElementById("resultado2").innerHTML = "acertou";
     } else {
       document.getElementById("resultado2").innerHTML = "errou";
     }
 
-    if (respostaUsuario3.value === "correto") {
+    if (respostaUsuario3.value == "correta") {
       document.getElementById("resultado3").innerHTML = "acertou";
     } else {
       document.getElementById("resultado3").innerHTML = "errou";
     }
 
-    if (respostaUsuario4.value === "correto") {
+    if (respostaUsuario4.value == "correta") {
       document.getElementById("resultado4").innerHTML = "acertou";
     } else {
       document.getElementById("resultado4").innerHTML = "errou";
