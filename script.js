e258da7a55faf20d214cac05711bd86857b76fc9
Rafael Henrique <127903349+RafaelHenriqueg2@users.noreply.github.com>
2023-11-08 08:09:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,76 @@
 function botar() {
-  var respostaUsuario1 = document.querySelector('input[name="resposta1"]:checked');
-  var respostaUsuario2 = document.querySelector('input[name="resposta2"]:checked');
-  var respostaUsuario3 = document.querySelector('input[name="resposta3"]:checked');
-  var respostaUsuario4 = document.querySelector('input[name="resposta4"]:checked');
+    if (
+        document.querySelector('input[name="resposta1"]:checked') &&
+        document.querySelector('input[name="resposta2"]:checked') &&
+        document.querySelector('input[name="resposta3"]:checked') &&
+        document.querySelector('input[name="resposta4"]:checked') &&
+        document.querySelector('input[name="resposta5"]:checked') &&
+        document.querySelector('input[name="resposta6"]:checked') &&
+        document.querySelector('input[name="resposta7"]:checked') &&
+        document.querySelector('input[name="resposta8"]:checked') &&
+        document.querySelector('input[name="resposta9"]:checked') &&
+        document.querySelector('input[name="resposta10"]:checked')
+    ) {
+        if (document.querySelector('input[name="resposta1"]:checked').value == "correta") {
+            document.getElementById("resultado_correto1").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado1").textContent = "Errou";
+        }
 
-  if (respostaUsuario1 && respostaUsuario2 && respostaUsuario3 && respostaUsuario4) {
-      if (respostaUsuario1.value == "correta") {
-          document.getElementById("resultado_correto1").innerHTML = "Acertou";
-      } else {
-          document.getElementById("resultado_errado1").innerHTML = "Errou";
-      }
+        if (document.querySelector('input[name="resposta2"]:checked').value == "correta") {
+            document.getElementById("resultado_correto2").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado2").textContent = "Errou";
+        }
 
-      if (respostaUsuario2.value == "correta") {
-          document.getElementById("resultado2").innerHTML = "Acertou";
-      } else {
-          document.getElementById("resultado2").innerHTML = "Errou";
-      }
+        if (document.querySelector('input[name="resposta3"]:checked').value == "correta") {
+            document.getElementById("resultado_correto3").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado3").textContent = "Errou";
+        }
 
-      if (respostaUsuario3.value == "correta") {
-          document.getElementById("resultado3").innerHTML = "Acertou";
-      } else {
-          document.getElementById("resultado3").innerHTML = "Errou";
-      }
+        if (document.querySelector('input[name="resposta4"]:checked').value == "correta") {
+            document.getElementById("resultado_correto4").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado4").textContent = "Errou";
+        }
 
-      if (respostaUsuario4.value == "correta") {
-          document.getElementById("resultado4").innerHTML = "Acertou";
-      } else {
-          document.getElementById("resultado4").innerHTML = "Errou";
+        if (document.querySelector('input[name="resposta5"]:checked').value == "correta") {
+            document.getElementById("resultado_correto5").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado5").textContent = "Errou";
+        }
 
-      }
-  } else {
-      alert("Não deixe nenhuma questão em aberto");
-  }
+        if (document.querySelector('input[name="resposta6"]:checked').value == "correta") {
+            document.getElementById("resultado_correto6").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado6").textContent = "Errou";
+        }
+
+        if (document.querySelector('input[name="resposta7"]:checked').value == "correta") {
+            document.getElementById("resultado_correto7").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado7").textContent = "Errou";
+        }
+
+        if (document.querySelector('input[name="resposta8"]:checked').value == "correta") {
+            document.getElementById("resultado_correto8").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado8").textContent = "Errou";
+        }
+
+        if (document.querySelector('input[name="resposta9"]:checked').value == "correta") {
+            document.getElementById("resultado_correto9").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado9").textContent = "Errou";
+        }
+
+        if (document.querySelector('input[name="resposta10"]:checked').value == "correta") {
+            document.getElementById("resultado_correto10").textContent = "Acertou";
+        } else {
+            document.getElementById("resultado_errado10").textContent = "Errou";
+        }
+    } else {
+        alert("Não deixe nenhuma questão em aberto");
+    }
 }
